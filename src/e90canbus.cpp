03859c53dc3138d73f181551479368a067c1fad5
Marcin648 <marcin648@gmail.com>
2019-02-17 17:16:50 +0100

    can-bus file rename

--- src/e90canbus.cpp
+++ src/e90canbus.cpp
@@ -6,19 +6,19 @@ uint16_t canCounter = 0;
 
 void canSend(){
   uint32_t courentTime = millis();
-  if(courentTime = lastTime > 100){
+  if(courentTime - lastTime > 100){
     //100 ms interval
-
+    printf("100ms\n");
     if(canCounter % 2 == 0){ //200 ms interval
-      ;
+      printf("200ms\n");;
     }
 
     if(canCounter % 5 == 0){ //500 ms interval
-      ;
+      printf("500ms\n");;
     }
 
     if(canCounter % 10 == 0){ //1000 ms interval
-      ;
+      printf("1000ms\n");;
     }
 
     canCounter++;
