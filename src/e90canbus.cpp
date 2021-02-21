0df589157089dae6e933f12c552c4474760cc108
Marcin648 <marcin648@gmail.com>
2021-02-21 19:37:26 +0100

    Change CAN interval

--- src/e90canbus.cpp
+++ src/e90canbus.cpp
@@ -9,31 +9,32 @@ uint16_t canCounter = 0;
 */
 void canSend(){
   uint32_t courentTime = millis();
-  if(courentTime - lastTime > 100){
-    //100 ms interval
+  if(courentTime - lastTime > 10){
+    //10 ms interval
     canSendIgnitionFrame();
     canSendRPM();
     canSendSpeed();
 
-    if(canCounter % 2 == 0){ //200 ms interval
+    if(canCounter % 20 == 0){ //200 ms interval
       canSendLights();
       canSendAbs();
       canSendEngineTemp();
 
       canSendAbsCounter();
       canSendAirbagCounter();
-    }
 
-    if(canCounter % 5 == 0){ //500 ms interval
-      canSendHandbrake();
       canSendFuel();
     }
 
-    if(canCounter % 6 == 0){ //600 ms interval
+    if(canCounter % 50 == 0){ //500 ms interval
+      canSendHandbrake();
+    }
+
+    if(canCounter % 60 == 0){ //600 ms interval
       canSendIndicator();
     }
 
-    if(canCounter % 10 == 0){ //1000 ms interval
+    if(canCounter % 100 == 0){ //1000 ms interval
       canSendTime();
     }
 
