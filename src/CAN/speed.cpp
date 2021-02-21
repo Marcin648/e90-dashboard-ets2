c7761720a6599b1a04abd4fdbfc11f0defcd97bc
Marcin648 <marcin648@gmail.com>
2021-02-21 19:37:51 +0100

    Improved speed accuracy

--- src/CAN/speed.cpp
+++ src/CAN/speed.cpp
@@ -4,16 +4,16 @@
 const uint16_t CAN_ID = 0x1A6;
 uint8_t speed_frame[8] = {0x13, 0x4D, 0x46, 0x4D, 0x33, 0x4D, 0xD0, 0xFF};
 
-uint16_t lastSpeed = 0;
+uint16_t last_speed_value = 0;
 void canSendSpeed(){
-  const uint8_t deltaTime = 100; // const
-  uint16_t speedValue = s_speed + lastSpeed;
+  const uint8_t delta_time = 100; // const
+  uint16_t speed_value = s_speed + last_speed_value;
 
   uint16_t counter = (speed_frame[6] | (speed_frame[7] << 8)) & 0x0FFF;
-  counter += deltaTime * DTIME_MULTIPLE;
+  counter += (float)delta_time * M_PI;
 
-  speed_frame[0] = speedValue;
-  speed_frame[1] = (speedValue >> 8);
+  speed_frame[0] = speed_value;
+  speed_frame[1] = (speed_value >> 8);
 
   speed_frame[2] = speed_frame[0];
   speed_frame[3] = speed_frame[1];
@@ -25,5 +25,5 @@ void canSendSpeed(){
   speed_frame[7] = (counter >> 8) | 0xF0;
 
   CAN.sendMsgBuf(CAN_ID, 0, 8, speed_frame);
-  lastSpeed = speedValue;
+  last_speed_value = speed_value;
 }
