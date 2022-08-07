93e63aefa4a66ca49885179f0786637763ed21a5
Marcin Jakubowski <marcin648@gmail.com>
2022-08-07 18:43:03 +0200

    Delete everything and add autowp can lib

--- include/serial.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "e90canbus.h"
-
-void serialReceive(const uint8_t* buffer, size_t size);
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,12 @@
-/*
-  Marcin648 2019
-  GNU General Public License 3.0
-*/
-
 #include <Arduino.h>
 
-#include <PacketSerial.h>
-#include <mcp_can.h>
 #include <SPI.h>
+#include <mcp2515.h>
 
-#include "e90canbus.h"
-#include "serial.h"
-#include "configuration.h"
+void setup(){
 
-/*
-  Set up can bus
-*/
-MCP_CAN CAN(SPI_CS_PIN);
-
-PacketSerial serial;
-
-/*
-  Main
-*/
-void setup() {
-  //Initialize serial port
-  serial.begin(SERIAL_BAUD_RATE);
-  serial.setPacketHandler(&serialReceive);
-
-  //Initialize CAN-BUS
-  while(CAN.begin(CAN_BAUD_RATE) != CAN_OK){
-    delay(100);
-  }
 }
 
-void loop() {
-  canSend();
-  serial.update();
+void loop(){
+
 }
